0d7240ea0ffad842dcc444e5b0ac3409d3e07a19
raju2327 <106509295+raju2327@users.noreply.github.com>
2022-05-30 19:26:29 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -6,7 +6,7 @@ var updateCallback = function(data){
     console.log("line : "+line);
     var movieName = line.text;
 	if (line.source.toLowerCase()==="visitor"){
-    var url = https://www.omdbapi.com?t=+movieName+&apikey=80a3537a;
+    var url = "https://www.omdbapi.com?t="+movieName+"&apikey=80a3537a";
     
    
     
