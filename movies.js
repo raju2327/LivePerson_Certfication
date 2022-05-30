d1d12dcc6939902b6505e559fee306930aa0406c
raju2327 <106509295+raju2327@users.noreply.github.com>
2022-05-30 12:36:03 +0530

    Added changes for OMDP API call

---
+++ movies.js
@@ -0,0 +1,45 @@
+var updateCall = function(data){
+    var value = data.newValue;
+    console.log("value : "+value);
+    var line = value[value.length -1];
+    console.log("line : "+line);
+    var movieName = line.text;
+	var key = "80a3537a";
+    if (line.source.toLowerCase()==="visitor"){
+    var url = "https://www.omdbapi.com?t="+movieName+"&apikey="+key;
+    
+    fetch(url)
+    .then(function(response){
+    
+    return response.json();
+    }
+    ).then(function(res){
+    document.getElementById("Title").innerHTML = res.Title;
+    document.getElementById("Year").innerHTML = res.Year;
+    document.getElementById("Genre").innerHTML = res.Genre;
+    document.getElementById("Plot").innerHTML = res.Plot;
+    document.getElementById("Director").innerHTML = res.Director;
+    document.getElementById("Poster").innerHTML = res.Poster;
+    
+       
+    }).catch(function(error){
+    console.log("Error Message from OMDB API : "+error);
+    })
+    }
+    };
+    
+    
+    
+    var notifyCall = function(error) {
+    if (err){
+    console.log("Error Occured in notification : "+err);
+    }
+    
+    errorMessage.innerHTML = "Unable to find the movie name you've entered...Please provide valid movie name";
+    };
+    
+    var chatData = "chatData.lines";
+    
+    lpTag.agentSDK.init({});
+    lpTag.agentSDK.bind(chatData, updateCall, notifyCall);
+
